3ac758641e91fed4a61b6016d38308c7cafef14f
Kirill Khudyakov <hudiakov@gmail.com>
2020-10-17 15:47:24 +0300

    [+] Flights screenshot

--- AviaSalesConcept/AviaSalesConcept/Screens/Search/SearchControl/SegmentControl/SegmentControl.h
+++ AviaSalesConcept/AviaSalesConcept/Screens/Search/SearchControl/SegmentControl/SegmentControl.h
@@ -11,6 +11,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SegmentControl : UIControl
 
+@property (copy) NSArray<UIButton*>* buttons;
+
+@property (nonatomic, nonnull) UIButton* selectedButton;
+
 @end
 
 NS_ASSUME_NONNULL_END
