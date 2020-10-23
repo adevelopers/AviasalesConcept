ae45cdfd2ddcf1407e58b71bb8f08cd5e44a8dc0
Kirill Khudyakov <hudiakov@gmail.com>
2020-10-24 00:43:11 +0300

    [+] Добавил DateHelper

---
+++ AviaSalesConcept/AviaSalesConcept/DateHelper.h
@@ -0,0 +1,18 @@
+//
+//  DateHelper.h
+//  AviaSalesConcept
+//
+//  Created by Kirill Khudiakov on 23.10.2020.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DateHelper : NSObject
+
++ (NSDateFormatter*)dateFormatter;
+    
+@end
+
+NS_ASSUME_NONNULL_END
